0b536ab85e581bd8dd021c94ada3ba80a551a787
Marco Mandelli <119368321+astro-marco@users.noreply.github.com>
2024-12-13 23:11:14 +0100

    Esercitazione 31/10 al minuto 1:05:32

---
+++ app/src/main/java/com/unimib/worldnews/PickCategoriesActivity.java
@@ -0,0 +1,43 @@
+package com.unimib.worldnews;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.card.MaterialCardView;
+
+public class PickCategoriesActivity extends AppCompatActivity {
+
+    public static final String TAG = PickCategoriesActivity.class.getName();
+    private MaterialCardView sportsCard;
+    private MaterialCardView healthCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pick_categories);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        sportsCard = findViewById(R.id.sports_card);
+        healthCard = findViewById(R.id.health_card);
+
+        sportsCard.setOnClickListener(view -> {
+            sportsCard.setChecked(!sportsCard.isChecked());
+        });
+
+        healthCard.setOnClickListener(view -> {
+            healthCard.setChecked(!healthCard.isChecked());
+        });
+    }
+}
