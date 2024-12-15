42c54384d7d4f2038a07da0ead4e6419861155d2
Marco Mandelli <119368321+astro-marco@users.noreply.github.com>
2024-12-15 03:09:09 +0100

    Esercitazione 7/11 fino a 47:38

--- app/src/main/java/com/unimib/worldnews/LoginActivity.java
+++ app/src/main/java/com/unimib/worldnews/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.unimib.worldnews;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -10,7 +12,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
+
+import org.apache.commons.validator.routines.EmailValidator;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -30,17 +35,29 @@ public class LoginActivity extends AppCompatActivity {
         Button loginButton = findViewById(R.id.loginButton);
 
         loginButton.setOnClickListener(view -> {
-            if (isEmailOk(editTextEmail.getText().toString()) && isPasswordOk(editTextPassword.getText().toString())) {
-                Log.d(TAG, "Launch new activity");
+            if (isEmailOk(editTextEmail.getText().toString())) {
+                if(isPasswordOk(editTextPassword.getText().toString())) {
+
+
+                } else {
+                    editTextPassword.setError("The password must have at least 8 chars");
+                    Snackbar.make(findViewById(android.R.id.content),
+                                    "Check your password", Snackbar.LENGTH_SHORT)
+                            .show();
+                }
             } else {
-                Log.d(TAG, "Error");
+                editTextEmail.setError("Check your email");
+                Snackbar.make(findViewById(android.R.id.content),
+                                "Insert a correct email", Snackbar.LENGTH_SHORT)
+                        .show();
             }
+
         });
 
     }
 
     private boolean isEmailOk(String email) {
-        return true;
+        return EmailValidator.getInstance().isValid(email);
     }
 
     private boolean isPasswordOk(String password) {
--- app/src/main/java/com/unimib/worldnews/PickCountryActivity.java
+++ app/src/main/java/com/unimib/worldnews/PickCountryActivity.java
@@ -4,12 +4,21 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+
 public class PickCountryActivity extends AppCompatActivity {
+
+    public static final String TAG = PickCountryActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_a_country);
 
+        try {
+            Log.d(TAG, savedInstanceState.getString("EMAIL_KEY"));
+        } catch (Exception e) {
+
+        }
     }
 }
